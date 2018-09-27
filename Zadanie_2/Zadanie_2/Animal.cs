cf2c737f407cbecff13444970d902701a8c4def0
dariuszkucharczyk <dariuszkucharczyk@o2.pl>
2018-09-27 07:31:19 +0200

    first commit

---
+++ Zadanie_2/Zadanie_2/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    public interface IAnimal
+    {
+        void Speak();
+    }
+
+    class Animal : IAnimal
+    {
+        public string name { get; set; }
+        public int nr { get; set; }
+
+        public Animal(int nr1, string name1)
+        {
+            nr = nr1;
+            name = name1;
+        }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal voice");
+        }
+
+        public void PrintNrlnPopulation()
+        {
+            Console.WriteLine(nr+ " "+ name);
+            Speak();
+
+        }
+
+    }
+
+    class Cat : Animal
+    {
+
+        public Cat(int nr1, string n1) : base(nr1, n1) { }
+        public override void Speak()
+        {
+            Console.WriteLine("Meow!");
+        }
+
+    }
+
+
+    class Dog : Animal
+    {
+
+        public Dog(int nr1, string n1) : base(nr1, n1) { }
+        public override void Speak()
+        {
+            Console.WriteLine("Bark!");
+        }
+    }
+
+}
---
+++ Zadanie_2/Zadanie_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> listAnimals = new List<Animal>();
+            listAnimals.Add(new Cat(1, "a"));
+            listAnimals.Add(new Cat(2, "b"));
+            listAnimals.Add(new Cat(3, "c"));
+            listAnimals.Add(new Dog(1, "d"));
+            listAnimals.Add(new Dog(2, "e"));
+            listAnimals.Add(new Dog(3, "f"));
+            foreach(Animal a1 in listAnimals)
+            {
+                a1.PrintNrlnPopulation();
+                    
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
